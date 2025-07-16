6fa660a279f9c51f029560d25f70737e5cd086ea
Daniel Steinigen <develop@steinigen.me>
2025-07-16 13:04:16 +0000

    feat(renderer): add renderer for chem CT

---
+++ structivize/renderers/chemistry/renderer_chem_ct.py
@@ -0,0 +1,12 @@
+
+from ...renderer import Renderer
+
+@Renderer.register("chem_ct")
+class RendererChemCt(Renderer):
+    DEFAULT_TOOL_CONFIGS = {
+        "obabel": {},
+    }
+
+    def _render_obabel(self):
+        self._execute_process(commands=["obabel", "-ict", self._filepath_code, "-O", f"{self.filepath_image}.svg", "-xb", "none", "-xd"])
+        self._svg_save(path=self.filepath_image)
