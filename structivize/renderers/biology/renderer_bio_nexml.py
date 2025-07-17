7e3755b892de95c342f838236c186b4798d96d20
Daniel Steinigen <develop@steinigen.me>
2025-07-17 08:20:43 +0000

    fest(renderer): add renderer for NEXML

---
+++ structivize/renderers/biology/renderer_bio_nexml.py
@@ -0,0 +1,29 @@
+
+import matplotlib.pyplot as plt
+from Bio import Phylo
+from ete3 import Tree, Nexml
+
+from ...renderer import Renderer
+
+
+@Renderer.register("bio_nexml")
+class RendererBioNexml(Renderer):
+    DEFAULT_TOOL_CONFIGS = {
+        "biopython": {},
+        "ete3": {},
+    }
+
+    def _render_biopython(self):
+        tree = Phylo.read(self._filepath_code, "nexml")
+        Phylo.draw(tree)
+        plt.savefig(f"{self.filepath_image}.png")
+        plt.close()
+
+    def _render_ete3(self):
+        nexml_project = Nexml() # TODO: not working
+        nexml_project.build_from_file("trees.xml")
+        tree_collections = nexml_project.get_trees()
+        collection_1 = tree_collections[0]
+        tree = collection_1.get_tree()[0]
+        # tree = Tree(self._filepath_code, format=1)
+        tree.render(f"{self.filepath_image}.png", w=800, dpi=300)
