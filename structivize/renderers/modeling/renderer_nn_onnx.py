81ecaaddeb3b0f5bd55ec040b1ca50777603f711
Daniel Steinigen <develop@steinigen.me>
2025-07-23 09:53:22 +0000

    feat(renderer): add renderer for ONNX

---
+++ structivize/renderers/modeling/renderer_nn_onnx.py
@@ -0,0 +1,12 @@
+from ...renderer import Renderer
+
+
+@Renderer.register("nn_onnx")
+class RendererNnOnnx(Renderer):
+    DEFAULT_TOOL_CONFIGS = {
+        "netron": {},
+    }
+
+    def _render_netron(self):
+        self._execute_process(commands=["netron_export", "--output", f"{self.filepath_image}.svg", self._filepath_code])
+        self._svg_save(self._filepath_image)
