d98b1d5597aa87c395ae0edcf9d7e048e780b5a5
Daniel Steinigen <develop@steinigen.me>
2025-07-23 09:52:44 +0000

    feat(renderer): add renderer for Smcat

---
+++ structivize/renderers/modeling/renderer_state_smcat.py
@@ -0,0 +1,21 @@
+from ...renderer import Renderer
+
+
+@Renderer.register("renderer_state_smcat")
+class RendererStateSmcat(Renderer):
+    DEFAULT_TOOL_CONFIGS = {
+        "smcat": {},
+        "smcat_dot": {},
+    }
+
+    def _render_smcat_dot(self):
+        self._execute_process(
+            commands=["smcat", "-T", "pdf", "-I", "smcat", self._filepath_code, "-o", f"{self.filepath_image}.pdf"]
+        )  # -d left-right
+        self._pdf_save(self.filepath_image)
+
+    def _render_smcat(self):
+        self._execute_process(
+            commands=["smcat", "-T", "pdf", "-I", "smcat", "-E", "circo", self._filepath_code, "-o", f"{self.filepath_image}.pdf"]
+        )
+        self._pdf_save(self.filepath_image)
