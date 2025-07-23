b812306b0eebf415ee4db03dc1fff70d1f92eab9
Daniel Steinigen <develop@steinigen.me>
2025-07-23 09:53:41 +0000

    feat(renderer): add renderer for Protobuf

---
+++ structivize/renderers/modeling/renderer_nn_protobuf.py
@@ -0,0 +1,16 @@
+from ...renderer import Renderer
+
+
+@Renderer.register("nn_protobuf")
+class RendererNnProtobuf(Renderer):
+    DEFAULT_TOOL_CONFIGS = {
+        "netron": {},
+    }
+
+    def preprocess_code(self):
+        self.clean_code_lines("{")
+        self._code = self._code.strip()
+
+    def _render_netron(self):
+        self._execute_process(commands=["netron_export", "--output", f"{self.filepath_image}.svg", self._filepath_code])
+        self._svg_save(self._filepath_image)
