ea52cacd1aac6fb6ba1497ced25541884c5de465
Daniel Steinigen <develop@steinigen.me>
2025-07-30 09:33:25 +0000

    feat(renderer): add renderer DBML

---
+++ src/structivize/renderers/datastructure/renderer_db_dbml.py
@@ -0,0 +1,19 @@
+from ...renderer import Renderer
+
+
+@Renderer.register("db_dbml")
+class RendererDbDbml(Renderer):
+    DEFAULT_TOOL_CONFIGS = {
+        "dbml_renderer": {},
+    }
+
+    def preprocess_code(self):
+        self.clean_code_lines("{")
+        self._code = self._code.strip()
+
+    def _render_dbml(self, filepath_code, filepath_image):
+        self._execute_process(commands=["dbml-renderer", "-i", filepath_code, "-o", f"{filepath_image}.svg"])
+        self._svg_save(path=filepath_image, scale=0.5)
+
+    def _render_dbml_renderer(self):
+        self._render_dbml(self._filepath_code, self.filepath_image)
