5f2f688797d5e2a5183c8182dcab744f1f11052f
Daniel Steinigen <develop@steinigen.me>
2025-07-31 08:20:06 +0000

    feat(renderer): add renderer for chess FEN

---
+++ src/structivize/renderers/culture/renderer_chess_fen.py
@@ -0,0 +1,23 @@
+import chess
+import chess.svg
+
+from ...renderer import Renderer
+
+
+@Renderer.register("chess_fen")
+class RendererChessFen(Renderer):
+    DEFAULT_TOOL_CONFIGS = {
+        "chess": {},
+    }
+
+    def preprocess_code(self):
+        self.clean_code_lines("/")
+        self._code = self._code.strip()
+
+    def verify_code(self):
+        return self._is_single_line() and "/" in self._code
+
+    def _render_chess(self):
+        board = chess.Board(self._code)
+        svg_code = chess.svg.board(board)
+        self._svg_save(path=self.filepath_image, svg_code=svg_code)
